f1d2c4d8ee71757c82cb8ee4ba6fe41a67830c92
Xuan0629 <xuan.zheng@ontariotechu.net>
2023-09-29 12:50:58 -0400

    #1

--- lab3_4/lib/main.dart
+++ lab3_4/lib/main.dart
@@ -7,119 +7,146 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'LAB-03&04',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'LAB-03&04'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+// The home page widget, containing the app bar and a list of tweets
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      // ListView.builder is used to generate a list of TweetWidgets
+      body: ListView.builder(
+        itemCount: 2,
+        itemBuilder: (context, index) {
+          switch (index) {
+            case 0:
+              return TweetWidget(
+                userIcon: 'T',
+                userShortName: 'TFT',
+                userLongName: 'Teamfight Tactics',
+                timeString: '2h ago',
+                description: "Chibi Champions! Chibi Champions are bite-sized versions of their League of Legends counterparts, with all the team-fighting talent and portal-making power of Little Legends. Making their debut on the Convergence first? Chibi Ekko, Chibi Jinx, and Chibi Vi!",
+                imageURL:"https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltd3a76db518246efb/6168ba3e892d6e03a88034e0/10182021-TFT_621_Chibi-Champs-Article-Header.jpg",
+                numComments: 5421,
+                numRetweets: 3098,
+                numLikes: 16685,
+              );
+            case 1:
+              return TweetWidget(
+                userIcon: 'N',
+                userShortName: 'NASA',
+                userLongName: 'NASA',
+                timeString: '4h ago',
+                description: "MOON!",
+                imageURL: 'https://www.nasa.gov/wp-content/uploads/2020/07/moon-near-side-lro.jpg?resize=768,768',
+                numComments: 5036,
+                numRetweets: 7801,
+                numLikes: 25680,
+              );
+          }
+        },
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class TweetWidget extends StatelessWidget {
+  final String userIcon;
+  final String userShortName;
+  final String userLongName;
+  final String timeString;
+  final String description;
+  final String imageURL;
+  final int numComments;
+  final int numRetweets;
+  final int numLikes;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const TweetWidget({
+    required this.userIcon,
+    required this.userShortName,
+    required this.userLongName,
+    required this.timeString,
+    required this.description,
+    required this.imageURL,
+    required this.numComments,
+    required this.numRetweets,
+    required this.numLikes,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    // Function to determine the background color of the avatar based on the user
+    Color determineAvatarColor() {
+      if (userShortName == 'TFT') {
+        return Colors.yellow;
+      } else if (userShortName == 'NASA') {
+        return Colors.blue;
+      } else {
+        return Colors.black;
+      }
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CircleAvatar(
+            backgroundColor: determineAvatarColor(),
+            child: Text(userIcon), // Make the text transparent as we have an image.
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(userLongName, style: TextStyle(fontWeight: FontWeight.bold)),
+                    const SizedBox(width: 6),
+                    Text('@' + userShortName, style: TextStyle(color: Colors.grey)),
+                    const SizedBox(width: 6),
+                    Text(timeString, style: TextStyle(color: Colors.grey)),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.expand_more))
+                  ],
+                ),
+                Text(description),
+                Image.network(imageURL, fit: BoxFit.cover),
+                Row(
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.chat_bubble_outline)),
+                    Text("$numComments"),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.repeat)),
+                    Text("$numRetweets"),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.favorite_border)),
+                    Text("$numLikes"),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.bookmark_border))
+                  ],
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
